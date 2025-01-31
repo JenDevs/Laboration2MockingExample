52aabec470e4100f5137d2527ae7d94f0d899152
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-01 00:55:50 +0100

    First commit in feature branch: Add SoppingCartTest and ShoppingCart

---
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ShoppingCartTest {
+
+    @Test
+    void addOneItemInShoppingCart() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+    }
+
+
+
+}
