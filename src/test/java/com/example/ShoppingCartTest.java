70b03b1fedd0d5b58a9a4370241faf1556bd60fd
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-01 13:54:19 +0100

    Add Item class and create Item in addOneItemInShoppingCart test

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -3,11 +3,16 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ShoppingCartTest {
 
     @Test
     void addOneItemInShoppingCart() {
         ShoppingCart shoppingCart = new ShoppingCart();
+        Item item = new Item();
+
+
     }
 
 
