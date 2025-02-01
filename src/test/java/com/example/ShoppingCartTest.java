e33138c63b10d808585763ebc48dfb1182aa3c09
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-01 15:28:19 +0100

    refactor: Add @BeforeEach and structured test variable

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,18 +1,27 @@
 package com.example;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShoppingCartTest {
+    private ShoppingCart shoppingCart;
+    private Item book;
+
+
+
+    @BeforeEach
+    public void setUp() {
+        shoppingCart = new ShoppingCart();
+        book = new Item();
+    }
 
     @Test
     void addOneItemInShoppingCart() {
-        ShoppingCart shoppingCart = new ShoppingCart();
-        Item item = new Item();
-        shoppingCart.addItem(item, 1);
-        assertEquals(1, item.getQuantity());
+        shoppingCart.addItem(book, 1);
+        assertEquals(1, book.getQuantity());
         assertEquals(1, shoppingCart.getTotalQuantity());
 
     }
