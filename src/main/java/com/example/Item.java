1d9f65b6e873a562008618db7e19450b5f03b51c
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-02 01:13:21 +0100

    refactor: Make name and price final in Item and removed unnecessary setters

--- src/main/java/com/example/Item.java
+++ src/main/java/com/example/Item.java
@@ -3,8 +3,8 @@ package com.example;
 public class Item {
 
     private int quantity;
-    private double price;
-    private String name;
+    private final double price;
+    private final String name;
 
     public Item(String name, double price) {
         this.price = price;
@@ -24,15 +24,8 @@ public class Item {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
