7c3c643ca900a722e14ed6f16dc932bc1e9e49de
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-02-02 21:41:21 +0100

    Add PaymentApiResponseImplemention class to implement the PaymentApiResponse interface

--- src/main/java/com/example/payment/PaymentApiResponse.java
+++ src/main/java/com/example/payment/PaymentApiResponse.java
@@ -1,5 +1,5 @@
 package com.example.payment;
 
 public interface PaymentApiResponse {
-    boolean isSuccess();
+    boolean success();
 }
---
+++ src/main/java/com/example/payment/PaymentApiResponseImplementation.java
@@ -0,0 +1,5 @@
+package com.example.payment;
+
+public record PaymentApiResponseImplementation(boolean success) implements PaymentApiResponse {
+
+}
